7e26e7d4010b1d7ffb6b7559ff9f21ef33d972e1
Disha <singhdisha1204@gmail.com>
2026-02-21 09:23:47 +0530

    Time: 8 ms (45.19%) | Memory: 42.6 MB (27.37%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -2,21 +2,20 @@ class Solution {
     public int countPrimeSetBits(int left, int right) {
         
 
-        // prime set of possible bit counts
-        // kyunki int me max 32 bits ho sakti hain
+    
         Set<Integer> primeSetBits = new HashSet<>(
             Arrays.asList(2,3,5,7,11,13,17,19,23,29,31)
         );
 
         int count = 0;
 
-        // left se right tak iterate karo
+    
         for (int num = left; num <= right; num++) {
 
-            // number of set bits count karo
+        
             int bits = Integer.bitCount(num);
 
-            // agar bits prime hai to answer badhao
+        
             if (primeSetBits.contains(bits)) {
                 count++;
             }
