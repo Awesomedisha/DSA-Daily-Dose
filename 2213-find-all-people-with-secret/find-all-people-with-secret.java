5730864be04244ef3e123705844d44cd564b28d4
Disha <singhdisha1204@gmail.com>
2026-03-11 23:25:33 +0530

    Time: 76 ms (82.91%) | Memory: 276.2 MB (55.57%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.java
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.java
@@ -1,90 +1,113 @@
 
 
-class Solution {
+class DSU {
 
     int[] parent;
+    int[] rank;
+
+    // Constructor
+    DSU(int n) {
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+
+        // Initially every node is its own parent
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    // Find parent with path compression
+    int findParent(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+
+        parent[node] = findParent(parent[node]);
+        return parent[node];
+    }
+
+    // Reset a node (disconnect it from group)
+    void resetNode(int node) {
+        parent[node] = node;
+        rank[node] = 0;
+    }
+
+    // Union by rank
+    void unionNodes(int node1, int node2) {
+
+        int parent1 = findParent(node1);
+        int parent2 = findParent(node2);
+
+        if (parent1 == parent2) {
+            return;
+        }
+
+        if (rank[parent1] > rank[parent2]) {
+            parent[parent2] = parent1;
+        }
+        else if (rank[parent1] < rank[parent2]) {
+            parent[parent1] = parent2;
+        }
+        else {
+            parent[parent1] = parent2;
+            rank[parent2]++;
+        }
+    }
+}
+
+class Solution {
 
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
 
+        List<Integer> ans = new ArrayList<>();
 
+        // Sort meetings based on time
+        Arrays.sort(meetings, (a, b) -> a[2] - b[2]);
 
+        DSU dsu = new DSU(n);
 
-
-
-
-    
-        parent = new int[n];
-
-
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-        }
-
-        
-        union(0, firstPerson);
-
-        
-        Arrays.sort(meetings, (a,b) -> a[2] - b[2]);
+        // Person 0 shares secret with firstPerson
+        dsu.unionNodes(0, firstPerson);
 
         int i = 0;
 
-        while(i < meetings.length){
+        while (i < meetings.length) {
 
             int time = meetings[i][2];
+            List<Integer> people = new ArrayList<>();
 
-            List<Integer> list = new ArrayList<>();
+            // Process meetings with same time
+            while (i < meetings.length && meetings[i][2] == time) {
 
-            int j = i;
+                int a = meetings[i][0];
+                int b = meetings[i][1];
 
-            
-            while(j < meetings.length && meetings[j][2] == time){
+                dsu.unionNodes(a, b);
 
-                int x = meetings[j][0];
-                int y = meetings[j][1];
+                people.add(a);
+                people.add(b);
 
-                union(x,y);
-
-                list.add(x);
-                list.add(y);
-
-                j++;
+                i++;
             }
 
-        
-            for(int p : list){
-                if(find(p) != find(0)){
-                    parent[p] = p; 
+            // Check who actually knows the secret
+            for (int person : people) {
+
+                if (dsu.findParent(person) != dsu.findParent(0)) {
+                    dsu.resetNode(person);
                 }
             }
-
-            i = j;
         }
 
-        List<Integer> ans = new ArrayList<>();
+        // Collect all people connected with 0
+        for (int person = 0; person < n; person++) {
 
-        for(int k = 0; k < n; k++){
-            if(find(k) == find(0)){
-                ans.add(k);
+            if (dsu.findParent(person) == dsu.findParent(0)) {
+                ans.add(person);
             }
         }
 
         return ans;
-    }
-
-    int find(int x){
-        if(parent[x] == x)
-            return x;
-
-        return parent[x] = find(parent[x]);
-    }
-
-    void union(int a, int b){
-
-        int pa = find(a);
-        int pb = find(b);
-
-        if(pa != pb){
-            parent[pb] = pa;
-        }
     }
 }
