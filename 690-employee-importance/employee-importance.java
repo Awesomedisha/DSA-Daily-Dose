85865c882c947015858e858fbde5f020ca20f597
Disha <singhdisha1204@gmail.com>
2026-03-20 22:01:44 +0530

    Time: 3 ms (55.62%) | Memory: 47.7 MB (92.54%) - LeetSync

--- 690-employee-importance/employee-importance.java
+++ 690-employee-importance/employee-importance.java
@@ -1,23 +1,23 @@
 class Solution {
     public int getImportance(List<Employee> employees, int id) {
         
-        // Step 1: Map create karna (id -> Employee)
+        
         Map<Integer, Employee> map = new HashMap<>();
         for (Employee e : employees) {
             map.put(e.id, e);
         }
         
-        // Step 2: DFS call
+     
         return dfs(id, map);
     }
     
-    // DFS function
+    
     private int dfs(int id, Map<Integer, Employee> map) {
         Employee emp = map.get(id);
         
-        int total = emp.importance; // khud ki importance
+        int total = emp.importance; 
         
-        // Step 3: sab subordinates ka importance add karo
+        
         for (int subId : emp.subordinates) {
             total += dfs(subId, map);
         }
