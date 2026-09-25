45755148337226d6d88e3ee84efaf03effcda7ac
Disha <singhdisha1204@gmail.com>
2026-09-26 00:02:15 +0530

    Time: 292 ms (57.39%), Space: 0B (100%) - LeetHub

---
+++ 3451-find-invalid-ip-addresses/3451-find-invalid-ip-addresses.sql
@@ -0,0 +1,9 @@
+SELECT
+    ip,
+    COUNT(*) AS invalid_count
+FROM logs
+WHERE NOT (
+    ip REGEXP '^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$'
+)
+GROUP BY ip
+ORDER BY invalid_count DESC, ip DESC;
